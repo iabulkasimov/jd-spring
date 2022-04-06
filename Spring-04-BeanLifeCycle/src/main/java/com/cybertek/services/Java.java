8bf0987da1968897e3849a91945bc04a849afaf8
iabulkasimov <iabulkasimov@gmail.com>
2022-04-06 16:44:01 -0400

    Bean LifeCycle

--- Spring-04-BeanLifeCycle/src/main/java/com/cybertek/services/Java.java
+++ Spring-04-BeanLifeCycle/src/main/java/com/cybertek/services/Java.java
@@ -6,4 +6,12 @@ public class Java implements Course {
     public void getTeachingHours() {
         System.out.println("Weekly Java teaching hours: 20");
     }
+
+    public void myInitMethod(){
+        System.out.println("Executing init method.");
+    }
+
+    public void myDestroyMethod(){
+        System.out.println("Executing destroy method.");
+    }
 }

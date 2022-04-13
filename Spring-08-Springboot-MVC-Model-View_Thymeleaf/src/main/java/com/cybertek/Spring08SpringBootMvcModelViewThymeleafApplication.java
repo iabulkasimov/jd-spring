2261e4a95a182a43f6c124a7c81efc71c10a6728
iabulkasimov <iabulkasimov@gmail.com>
2022-04-13 09:47:19 -0400

    Springboot MVC Model View

---
+++ Spring-08-Springboot-MVC-Model-View_Thymeleaf/src/main/java/com/cybertek/Spring08SpringBootMvcModelViewThymeleafApplication.java
@@ -0,0 +1,13 @@
+package com.cybertek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring08SpringBootMvcModelViewThymeleafApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring08SpringBootMvcModelViewThymeleafApplication.class, args);
+    }
+
+}

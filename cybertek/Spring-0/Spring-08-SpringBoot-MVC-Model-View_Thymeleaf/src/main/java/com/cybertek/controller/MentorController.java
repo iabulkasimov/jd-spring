b3413f52642ee60b510669dfe3ec34e9e6979e33
iabulkasimov <iabulkasimov@gmail.com>
2022-04-13 09:19:37 -0400

    Springboot MVC Model View

---
+++ cybertek/Spring-0/Spring-08-SpringBoot-MVC-Model-View_Thymeleaf/src/main/java/com/cybertek/controller/MentorController.java
@@ -0,0 +1,28 @@
+package com.cybertek.controller;
+
+import com.cybertek.enums.Gender;
+import com.cybertek.model.Mentor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+@RequestMapping("/mentor")
+public class MentorController {
+
+    @GetMapping("/list")
+    public String showTable(Model model){
+        List<Mentor> mentorList = new ArrayList<>();
+        mentorList.add(new Mentor("Mike","Smith",45, Gender.MALE));
+        mentorList.add(new Mentor("Ike","Abu",40, Gender.MALE));
+        mentorList.add(new Mentor("Ammy","Cruse",50, Gender.FEMALE));
+
+        model.addAttribute("mentors", mentorList);
+
+        return "mentor/mentor-list";
+    }
+}

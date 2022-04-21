16fcf857c2f22c19d934b9a24a317e95f363c2b8
iabulkasimov <iabulkasimov@gmail.com>
2022-04-21 17:11:49 -0400

    Springboot ORM Table Creation

--- Spring-09-ORM-TableCreation/src/main/java/com/cybertek/Spring09OrmTableCreationApplication.java
+++ Spring-09-ORM-TableCreation/src/main/java/com/cybertek/Spring09OrmTableCreationApplication.java
@@ -1,13 +1,47 @@
 package com.cybertek;
 
+import com.cybertek.entity.Car;
+import com.cybertek.repository.CarRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.annotation.PostConstruct;
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
 public class Spring09OrmTableCreationApplication {
 
+    @Autowired
+    CarRepository carRepository;
+
     public static void main(String[] args) {
         SpringApplication.run(Spring09OrmTableCreationApplication.class, args);
+    }
+
+    @PostConstruct
+    public void dataInit(){
+
+        List<Car> cars = new ArrayList<>();
+
+        cars.add(new Car("BMW", "M3"));
+        cars.add(new Car("AUDI", "Q5"));
+        cars.add(new Car("BMW", "M7"));
+        cars.add(new Car("KIA", "Sanata"));
+
+        carRepository.saveAll(cars);
+
+        Car car1 = new Car("BMW", "M5");
+        Car car2 = new Car("AUDI", "Q7");
+        Car car3 = new Car("BMW", "M3");
+        Car car4 = new Car("KIA", "Sorento");
+
+        carRepository.save(car1);
+        carRepository.save(car2);
+        carRepository.save(car3);
+        carRepository.save(car4);
+
     }
 
 }

c1ea0eab0a16399e4238cc8a1516edc77028f055
iabulkasimov <iabulkasimov@gmail.com>
2022-04-21 16:29:06 -0400

    Springboot ORM Table Creation

---
+++ Spring-09-ORM-TableCreation/src/main/resources/data2.sql
@@ -0,0 +1,4 @@
+INSERT INTO students(first_name,last_name,email) VALUES('Mike','Smith','mike@cybertek.com');
+INSERT INTO students(first_name,last_name,email) VALUES('Lena','Brown','lena@cybertek.com');
+INSERT INTO students(first_name,last_name,email) VALUES('Mila','Dance','mila@cybertek.com');
+INSERT INTO students(first_name,last_name,email) VALUES('Suzan','Ran','suzan@cybertek.com');

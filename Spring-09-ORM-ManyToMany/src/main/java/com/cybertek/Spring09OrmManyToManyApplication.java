fef7013f1913d2c7980f054afb9c1e93e7d3c8b2
iabulkasimov <iabulkasimov@gmail.com>
2022-04-22 16:09:03 -0400

    ORM Mapping ManyToMany

---
+++ Spring-09-ORM-ManyToMany/src/main/java/com/cybertek/Spring09OrmManyToManyApplication.java
@@ -0,0 +1,13 @@
+package com.cybertek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring09OrmManyToManyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring09OrmManyToManyApplication.class, args);
+    }
+
+}
---
+++ Spring-09-ORM-ManyToMany/src/main/java/com/cybertek/entiry/Tag.java
@@ -0,0 +1,29 @@
+package com.cybertek.entiry;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class Tag {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @ManyToMany(mappedBy = "tags")
+    private Set<Tag> tags = new HashSet<>();
+
+    public Tag(String name) {
+        this.name = name;
+    }
+}

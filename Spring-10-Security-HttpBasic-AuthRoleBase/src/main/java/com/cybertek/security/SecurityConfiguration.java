a7ce0778b199beeaff4b8bf31c2ee170c5075764
iabulkasimov <iabulkasimov@gmail.com>
2022-05-08 16:08:31 -0400

    Security - HttpBasic AuthRoleBase

--- Spring-10-Security-HttpBasic-AuthRoleBase/src/main/java/com/cybertek/security/SecurityConfiguration.java
+++ Spring-10-Security-HttpBasic-AuthRoleBase/src/main/java/com/cybertek/security/SecurityConfiguration.java
@@ -16,7 +16,10 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.
                 authorizeRequests()//request should be authorized
-                .anyRequest().authenticated()//incoming request be authenticated
+                .antMatchers("index.html").permitAll()
+                .antMatchers("/profile/**").authenticated()
+                .antMatchers("/admin/**").hasRole("ADMIN")
+                .antMatchers("/management/**").hasAnyRole("ADMIN","MANAGER")
                 .and()
                 .httpBasic();//perform basic http authentication
     }
@@ -27,7 +30,9 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .inMemoryAuthentication()
                 .withUser("admin").password(passwordEncoder().encode("admin123")).roles("ADMIN")
                 .and()
-                .withUser("user").password(passwordEncoder().encode("user123")).roles("USER");
+                .withUser("user").password(passwordEncoder().encode("user123")).roles("USER")
+                .and()
+                .withUser("manager").password(passwordEncoder().encode("manager123")).roles("MANAGER");
     }
 
     @Bean

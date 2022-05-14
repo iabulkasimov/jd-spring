13bfd5bbfdd338ad1ed10bb7744fea03fb43072c
iabulkasimov <iabulkasimov@gmail.com>
2022-05-13 20:22:39 -0400

    Spring Rest - School Lab

---
+++ Spring-11-Rest-SchoolLab/src/main/java/com/cybertek/model/Student.java
@@ -0,0 +1,36 @@
+package com.cybertek.model;
+
+import com.cybertek.enums.Status;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "student")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonIgnoreProperties(value = {"hibernateLazyInitializer"}, ignoreUnknown = true)
+public class Student extends BaseEntiry{
+
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String username;
+
+    @Enumerated(EnumType.STRING)
+    private Status status;
+
+    @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @JoinColumn(name = "address_id")
+    private Address address;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "parent_id")
+    private Parent parent;
+}

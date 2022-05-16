1859a019e20bc3d4adb79d1ec6022129816e40ae
iabulkasimov <iabulkasimov@gmail.com>
2022-05-16 10:44:08 -0400

    Spring Rest - Webclient

--- Spring-11-Rest-Jackson/src/main/java/com/cybertek/repository/GenreRepository.java
+++ Spring-11-Rest-Jackson/src/main/java/com/cybertek/repository/GenreRepository.java
@@ -21,4 +21,7 @@ public interface GenreRepository extends JpaRepository<Genre,Long> {
     //Write a native query that returns genres by containing name
     @Query(value = "SELECT * FROM genre WHERE name ILIKE concat('%',?1,'%')",nativeQuery = true)
     List<Genre> retrieveByName(String name);
+
+    @Query(value = "SELECT count(*) FROM genre g movie_genre_rel mgr on g.id = mgr.genre_id WHERE g.id ?=", nativeQuery = true)
+    Integer countGenresNativeQuery(Long id);
 }

9ad14b6972b218342259c3ab74ab8fa708ca2c14
iabulkasimov <iabulkasimov@gmail.com>
2022-05-25 15:09:55 -0400

    Spring Rest - Security JWT

--- Spring-11-Rset-Security-JWT/src/main/java/com/cybertek/Spring11RsetSecurityJwtApplication.java
+++ Spring-11-Rset-Security-JWT/src/main/java/com/cybertek/Spring11RsetSecurityJwtApplication.java
@@ -2,12 +2,19 @@ package com.cybertek;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class Spring11RsetSecurityJwtApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(Spring11RsetSecurityJwtApplication.class, args);
+    }
+
+    @Bean
+    BCryptPasswordEncoder bCryptPasswordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 
 }

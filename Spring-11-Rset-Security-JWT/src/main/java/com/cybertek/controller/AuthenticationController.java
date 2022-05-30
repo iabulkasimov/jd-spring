1725940f46d5dcc935f558ea6e2be8d68c84f9f5
iabulkasimov <iabulkasimov@gmail.com>
2022-05-30 13:06:02 -0400

    Spring Rest - Security JWT

--- Spring-11-Rset-Security-JWT/src/main/java/com/cybertek/controller/AuthenticationController.java
+++ Spring-11-Rset-Security-JWT/src/main/java/com/cybertek/controller/AuthenticationController.java
@@ -1,8 +1,10 @@
 package com.cybertek.controller;
 
+import com.cybertek.annotation.DefaultExceptionMessage;
 import com.cybertek.entity.AuthenticationRequest;
 import com.cybertek.entity.ResponseWrapper;
 import com.cybertek.entity.User;
+import com.cybertek.exception.ServiceException;
 import com.cybertek.service.UserService;
 import com.cybertek.util.JWTUtil;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,6 +27,7 @@ public class AuthenticationController {
 
 
     @PostMapping("/authenticate")
+    @DefaultExceptionMessage(defaultMessage = "Bad Credentials.")
     public ResponseEntity<ResponseWrapper> doLogin(@RequestBody AuthenticationRequest authenticationRequest){
 
         String password = authenticationRequest.getPassword();
@@ -38,6 +41,11 @@ public class AuthenticationController {
         String jwtToken = jwtUtil.generateToken(foundUser);
 
         return ResponseEntity.ok(new ResponseWrapper("Login Succesfull!",jwtToken));
+    }
 
+    @PostMapping("/create-user")
+    public ResponseEntity<ResponseWrapper> createAccount(@RequestBody User user) throws ServiceException {
+        User createUser = userService.createUser(user);
+        return ResponseEntity.ok(new ResponseWrapper("User has been created successfully",createUser));
     }
 }
